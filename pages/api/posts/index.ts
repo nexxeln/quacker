801411db0d1729db2e518b737b07e4b65caacfd3
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-30 00:46:32 +0530

    create get endpoint and follow REST standards

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -1,14 +1,30 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import prisma from "../../lib/prisma";
+import prisma from "../../../lib/prisma";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") {
+  if (req.method === "GET") {
+    return await getPosts(req, res);
+  } else if (req.method === "POST") {
     return await createPost(req, res);
   } else {
     return res.status(405).json({ message: "Method not allowed" });
+  }
+}
+
+async function getPosts(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const posts = await prisma.post.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return res.status(200).json(posts);
+  } catch (error) {
+    console.error("Request error", error);
+    return res.status(500).json({ error: "Error fetching posts" });
   }
 }
 
