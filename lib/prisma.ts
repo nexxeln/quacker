b79b9d6716e06d3fad79efe7988366bc3b409c0b
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-29 23:20:31 +0530

    format

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,12 +5,15 @@ declare global {
 }
 
 let prisma: PrismaClient;
+
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
     global.prisma = new PrismaClient();
   }
+
   prisma = global.prisma;
 }
+
 export default prisma;
