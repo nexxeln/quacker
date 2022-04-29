131d29187390e21473d61a8d6fa80c6ce3466aaf
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-29 14:19:30 +0530

    organise components

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Heading from "../components/Heading";
+import Heading from "../components/Home/Heading";
 
 const Home: NextPage = () => {
   return (
