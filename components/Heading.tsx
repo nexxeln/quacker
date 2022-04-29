778cd4e53278f01a3a119f5b803a521063a77c69
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-29 14:16:13 +0530

    create heading component and home page

---
+++ components/Heading.tsx
@@ -0,0 +1,40 @@
+import { createStyles, Text } from "@mantine/core";
+
+const BREAKPOINT = "@media (max-width: 768px)";
+
+const useStyles = createStyles((theme) => ({
+  title: {
+    fontFamily: `Graycliff CF, ${theme.fontFamily}`,
+    fontSize: 62,
+    fontWeight: 900,
+    lineHeight: 1.1,
+    margin: 0,
+    padding: 0,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+    [BREAKPOINT]: {
+      fontSize: 42,
+      lineHeight: 1.2,
+    },
+  },
+}));
+
+const Heading = () => {
+  const { classes, cx } = useStyles();
+  return (
+    <div className={classes.title}>
+      <Text
+        component="span"
+        variant="gradient"
+        gradient={{ from: "#db9544", to: "#c97f28" }}
+        inherit
+      >
+        Quacker
+      </Text>
+    </div>
+  );
+};
+
+export default Heading;
---
+++ pages/home.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Heading from "../components/Heading";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Quacker</title>
+        <meta name="description" content="Just quack it!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Heading />
+    </div>
+  );
+};
+
+export default Home;
