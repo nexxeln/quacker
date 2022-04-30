f25c2654f7f061a7b525a7f6792076ac1bd72eea
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-30 23:22:29 +0530

    make post component reusable

--- components/Home/Posts/Post.tsx
+++ components/Home/Posts/Post.tsx
@@ -4,16 +4,21 @@ import { PostTypes } from "../../../lib/fetcher";
 
 type PostProps = {
   data: PostTypes;
+  lineClamp?: boolean;
 };
 
-const Post: FC<PostProps> = ({ data }) => {
+const Post: FC<PostProps> = ({ data, lineClamp }) => {
   return (
     <>
       <Paper shadow="md" radius="md" p="md" mb="xl" withBorder>
         <Title pb={20} sx={{ lineHeight: 1.15 }}>
           {data.title}
         </Title>
-        <Text lineClamp={3}>{data.body}</Text>
+        {lineClamp ? (
+          <Text lineClamp={3}>{data.body}</Text>
+        ) : (
+          <Text>{data.body}</Text>
+        )}
       </Paper>
     </>
   );
